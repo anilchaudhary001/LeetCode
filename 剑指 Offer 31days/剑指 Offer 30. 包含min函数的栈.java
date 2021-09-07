8d47a87c5be258e648995e4df314203f69d7fd84
AaronPhantomhive <44737094+AaronPhantomhive@users.noreply.github.com>
2021-09-07 21:12:33 +0800

    Create 剑指 Offer 30. 包含min函数的栈.java

---
+++ 剑指 Offer 31days/剑指 Offer 30. 包含min函数的栈.java
@@ -0,0 +1,60 @@
+// Day 1
+
+/**
+定义栈的数据结构，请在该类型中实现一个能够得到栈的最小元素的 min 函数在该栈中，调用 min、push 及 pop 的时间复杂度都是 O(1)。
+
+ 
+
+示例:
+
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.min();   --> 返回 -3.
+minStack.pop();
+minStack.top();      --> 返回 0.
+minStack.min();   --> 返回 -2.
+ 
+
+提示：
+
+各函数的调用总次数不超过 20000 次
+*/
+
+// 官方solution
+// 执行用时：17 ms, 在所有 Java 提交中击败了93.87%的用户
+// 内存消耗：39.9 MB, 在所有 Java 提交中击败了96.00%的用户
+
+class MinStack {
+    Stack<Integer> A, B;
+    public MinStack() {
+        A = new Stack<>();
+        B = new Stack<>();
+    }
+    public void push(int x) {
+        A.add(x);
+        if(B.empty() || B.peek() >= x)
+            B.add(x);
+    }
+    public void pop() {
+        if(A.pop().equals(B.peek()))
+            B.pop();
+    }
+    public int top() {
+        return A.peek();
+    }
+    public int min() {
+        return B.peek();
+    }
+}
+
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.min();
+ */
