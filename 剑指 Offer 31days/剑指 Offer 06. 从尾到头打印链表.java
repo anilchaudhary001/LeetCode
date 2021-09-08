7b60156f1ae36e292958c4d11f05cd494b5c4955
AaronPhantomhive <44737094+AaronPhantomhive@users.noreply.github.com>
2021-09-08 20:23:06 +0800

    Create 剑指 Offer 06. 从尾到头打印链表.java

---
+++ 剑指 Offer 31days/剑指 Offer 06. 从尾到头打印链表.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int[] reversePrint(ListNode head) {
+        ListNode node = head;
+        int count = 0;
+        while (node != null) {
+            count++;
+            node = node.next;
+        }
+        int[] nums = new int[count];
+        node = head;
+        for (int i = count - 1; i >= 0; --i) {
+            nums[i] = node.val;
+            node = node.next;
+        }
+        return nums;
+    }
+}
