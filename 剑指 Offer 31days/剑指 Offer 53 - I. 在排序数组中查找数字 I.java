5a7c678c9ad6fec1720e239fa1a5d4f3f7099a8f
AaronPhantomhive <44737094+AaronPhantomhive@users.noreply.github.com>
2021-09-13 22:17:28 +0800

    Create 剑指 Offer 53 - I. 在排序数组中查找数字 I.java

---
+++ 剑指 Offer 31days/剑指 Offer 53 - I. 在排序数组中查找数字 I.java
@@ -0,0 +1,33 @@
+/**
+统计一个数字在排序数组中出现的次数。
+
+示例 1:
+输入: nums = [5,7,7,8,8,10], target = 8
+输出: 2
+
+示例 2:
+输入: nums = [5,7,7,8,8,10], target = 6
+输出: 0
+ 
+提示：
+
+0 <= nums.length <= 105
+-109 <= nums[i] <= 109
+nums 是一个非递减数组
+-109 <= target <= 109
+*/
+
+// 解题1：暴力拆解
+// 过一遍循环，用一个变量计数。
+// 时间复杂度O(n), 空间复杂度O(1)
+class Solution {
+    public int search(int[] nums, int target) {
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if(target == nums[i]) {
+                result++;
+            }
+        }
+        return result;
+    }
+}
