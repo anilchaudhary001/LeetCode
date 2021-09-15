e2be88eaf15c172e7288547819a5f77a051640e1
AaronPhantomhive <44737094+AaronPhantomhive@users.noreply.github.com>
2021-09-15 21:17:09 +0800

    Create 剑指 Offer 32 - I. 从上到下打印二叉树.java

---
+++ 剑指 Offer 31days/剑指 Offer 32 - I. 从上到下打印二叉树.java
@@ -0,0 +1,44 @@
+/**
+从上到下打印出二叉树的每个节点，同一层的节点按照从左到右的顺序打印。
+
+例如:
+给定二叉树: [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回：
+
+[3,9,20,15,7]
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int[] levelOrder(TreeNode root) {
+        if(root == null) return new int[0];
+        Queue<TreeNode> queue = new LinkedList<>(){{ add(root); }};
+        ArrayList<Integer> ans = new ArrayList<>();
+        while(!queue.isEmpty()) {
+            TreeNode node  = queue.poll();
+            ans.add(node.val);
+            System.out.println(node.val);
+            if(node.left != null) queue.add(node.left);
+            if(node.right != null) queue.add(node.right);
+        }
+        int[] result = new int[ans.size()];
+        for(int i = 0; i < ans.size(); i++) {
+            result[i] = ans.get(i);
+        }
+        return result;
+    }
+}
