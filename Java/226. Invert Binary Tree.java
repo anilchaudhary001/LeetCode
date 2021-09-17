43ae0691099e5a4a77e6cf91f962fad5960e2359
AaronPhantomhive <44737094+AaronPhantomhive@users.noreply.github.com>
2021-09-17 21:27:32 +0800

    Create 226. Invert Binary Tree.java

---
+++ Java/226. Invert Binary Tree.java
@@ -0,0 +1,46 @@
+/**
+Given the root of a binary tree, invert the tree, and return its root.
+
+Example 1:
+Input: root = [4,2,7,1,3,6,9]
+Output: [4,7,2,9,6,3,1]
+
+Example 2:
+Input: root = [2,1,3]
+Output: [2,3,1]
+
+Example 3:
+Input: root = []
+Output: []
+*/
+
+// Recursion solution
+// 执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
+// 内存消耗：35.5 MB, 在所有 Java 提交中击败了96.26%的用户
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode temp = root.right;
+        root.right = root.left;
+        root.left = temp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
