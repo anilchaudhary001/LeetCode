f3685870dbba91f8acdecb273e13d2fd91dccebc
AaronPhantomhive <44737094+AaronPhantomhive@users.noreply.github.com>
2021-09-18 21:14:00 +0800

    Create 101. Symmetric Tree.java

---
+++ Java/101. Symmetric Tree.java
@@ -0,0 +1,44 @@
+/**
+Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+
+Example 1:
+Input: root = [1,2,2,3,4,4,3]
+Output: true
+
+Example 2:
+Input: root = [1,2,2,null,3,null,3]
+Output: false
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        // if (root == null) return true;
+        return isMirror(root, root);
+    }
+    public boolean isMirror (TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) {
+            return true;
+        }
+        if (t1 == null || t2 == null) {
+            return false;
+        }
+        // 这个地方注意是tree 1 和 tree 2 的值对比，不是tree1和tree2对比。
+        return (t1.val == t2.val) && isMirror(t1.left, t2.right) && isMirror(t1.right, t2.left);
+    }
+
+}
