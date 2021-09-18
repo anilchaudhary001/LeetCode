6d704795bef812657ed3663ee84ccf48e2edd483
AaronPhantomhive <44737094+AaronPhantomhive@users.noreply.github.com>
2021-09-18 21:50:41 +0800

    Update 145. 二叉树的后序遍历.java

--- Data Structure/Binary tree/145. 二叉树的后序遍历.java
+++ Data Structure/Binary tree/145. 二叉树的后序遍历.java
@@ -12,3 +12,18 @@
 
 输出: [3,2,1]
 */
+
+// Recursion solution
+
+class Solution {
+    List<Integer> list = new ArrayList<Integer>();
+    public List<Integer> postorderTraversal(TreeNode root) {
+        if (root == null) return list;
+        postorderTraversal(root.left);
+        postorderTraversal(root.right);
+        list.add(root.val);
+        return list;
+    }
+}
+
+// Iteration solution
