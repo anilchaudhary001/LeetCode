963fcb8c724c728acd814e7d52e74df690c27771
AaronPhantomhive <44737094+AaronPhantomhive@users.noreply.github.com>
2021-09-18 21:38:13 +0800

    Create 144. Binary Tree Preorder Traversal.java

---
+++ Java/144. Binary Tree Preorder Traversal.java
@@ -0,0 +1,40 @@
+/**
+Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+Example 1:
+Input: root = [1,null,2,3]
+Output: [1,2,3]
+
+Example 2:
+Input: root = []
+Output: []
+
+Example 3:
+Input: root = [1]
+Output: [1]
+
+Example 4:
+Input: root = [1,2]
+Output: [1,2]
+
+Example 5:
+Input: root = [1,null,2]
+Output: [1,2]
+*/
+
+// Recursion solution
+
+class Solution {
+    List<Integer> list = new ArrayList<Integer>();
+    public List<Integer> preorderTraversal(TreeNode root) {
+        if (root == null) return list;
+        list.add(root.val);
+        preorderTraversal(root.left);
+        preorderTraversal(root.right);
+        return list;
+    }
+}
+
+// Iteration solution
+
+
