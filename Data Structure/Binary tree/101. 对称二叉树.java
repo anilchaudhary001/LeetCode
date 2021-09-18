b6a1b5af2b4aac2bdc5e03d7630b5f47aaf1e5e0
AaronPhantomhive <44737094+AaronPhantomhive@users.noreply.github.com>
2021-09-18 21:12:27 +0800

    Create 101. 对称二叉树.java

---
+++ Data Structure/Binary tree/101. 对称二叉树.java
@@ -0,0 +1,50 @@
+/**
+给定一个二叉树，检查它是否是镜像对称的。
+例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+
+但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+
+    1
+   / \
+  2   2
+   \   \
+   3    3
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        // if (root == null) return true;
+        return isMirror(root, root);
+    }
+    public boolean isMirror (TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) {
+            return true;
+        }
+        if (t1 == null || t2 == null) {
+            return false;
+        }
+        // 这个地方注意是tree 1 和 tree 2 的值对比，不是tree1和tree2对比。
+        return (t1.val == t2.val) && isMirror(t1.left, t2.right) && isMirror(t1.right, t2.left);
+    }
+
+}
