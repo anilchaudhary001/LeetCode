274ecff176f171a1ebeb8fa0e2de39ab9935a8ba
AaronPhantomhive <44737094+AaronPhantomhive@users.noreply.github.com>
2021-09-19 15:32:28 +0800

    Create 111. 二叉树的最小深度.java

---
+++ Data Structure/Binary tree/111. 二叉树的最小深度.java
@@ -0,0 +1,41 @@
+/**
+给定一个二叉树，找出其最小深度。
+
+最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+
+说明：叶子节点是指没有子节点的节点。
+
+示例 1：
+输入：root = [3,9,20,null,null,15,7]
+输出：2
+
+示例 2：
+输入：root = [2,null,3,null,4,null,5,null,6]
+输出：5
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        if (root.left == null && root.right == null) return 1;
+        int m1 = minDepth(root.left);
+        int m2 = minDepth(root.right);
+        if (root.left == null || root.right == null) return m1 + m2 + 1;
+        return Math.min(m1, m2) + 1; 
+    }
+}
